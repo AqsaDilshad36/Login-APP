377f6190f67c033ba24b4ecedf059247329d79c6
AqsaDilshad36 <168129179+AqsaDilshad36@users.noreply.github.com>
2024-05-02 13:49:48 +0500

    aqsa

--- app/src/main/java/com/example/signup/SplashScreenActivity.java
+++ app/src/main/java/com/example/signup/SplashScreenActivity.java
@@ -1,6 +1,8 @@
 package com.example.signup;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,16 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash_screen);
+
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreenActivity.this,loginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);//For Event start after delay
         
     }
 }
